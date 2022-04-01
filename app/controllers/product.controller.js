e9f0b8ce9bc10c2d19a24ebf6ab79c8b5e99a07b
Jan Willem <s1109221@student.hsleiden.nl>
2022-04-01 16:13:13 +0100

    clean up and finalizations

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -121,3 +121,25 @@ exports.deleteAll = (req, res) => {
     });
   };
 
+  exports.update = (req, res) => {
+    const id = req.params.id;
+    Product.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "User was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update product with id ${id}. Maybe product was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating User with id=" + id
+        });
+      });
+  };
